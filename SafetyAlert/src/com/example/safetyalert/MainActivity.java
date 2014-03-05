2e9674932839c9df35befc5404141147e30ae697
SourceDave <david.d.li@gmail.com>
2014-03-04 22:27:29 -0800

    Added DisplayMessageActivity and intents.

--- SafetyAlert/src/com/example/safetyalert/MainActivity.java
+++ SafetyAlert/src/com/example/safetyalert/MainActivity.java
@@ -1,17 +1,21 @@
 package com.example.safetyalert;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
-    @Override
+    public static final String EXTRA_MESSAGE = "com.example.safetyalert.MESSAGE";
+
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -20,4 +24,11 @@ public class MainActivity extends Activity {
         return true;
     }
     
+    public void sendMessage(View view) {
+    	Intent intent = new Intent(this, DisplayMessageActivity.class);
+    	EditText editText = (EditText) findViewById(R.id.edit_message);
+    	String message = editText.getText().toString();
+    	intent.putExtra(EXTRA_MESSAGE, message);
+    	startActivity(intent);
+    }
 }
